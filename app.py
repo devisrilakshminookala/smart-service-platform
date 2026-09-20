2e63f15d2655493100fed2acda970e46b144f272
Devi Sri Lakshmi <devisrilakshminookala@gmail.com>
2026-09-20 19:51:25 +0530

    Add Docker deployment files

--- app.py
+++ app.py
@@ -455,4 +455,4 @@ def assign_staff(request_id):
 # ---------------- RUN APPLICATION ----------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
